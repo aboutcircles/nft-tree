b4a8bfdaaf840cca033db7434b74ffb6e9f033c5
Luka la d <lukas-8@live.fr>
2024-04-23 17:59:40 +0200

    fix: total supply type

--- nft-tree-frontend/hooks/use-donations.ts
+++ nft-tree-frontend/hooks/use-donations.ts
@@ -5,28 +5,28 @@ import { Address } from "viem";
 
 function useDonations() {
   const [totalSupply, setTotalSupply] = useState(0);
-  const [donors, setDonors] = useState<Address[]>(["0x3246012BC7c9Bf13Dc00166Cf969467Ef93bf7E5", "0xB72b9865a922305Af83803472d6054d4488B9Cc4", "0x0ea8f917a20D33F133aad1189eAE3B4d5601fC19"]);
+  const [donors, setDonors] = useState<Address[]>([]);
 
   useEffect(() => {
     async function fetchTotalSupply() {
       try {
         const result = await publicClient.readContract({
-          address: "0x0",
+          address: "0x992d9e030fF8187F950298f89Fb52745764bdEEF",
           abi: circlesTreeABI,
           functionName: "totalSupply",
         });
-        setTotalSupply(result);
+        setTotalSupply(Number(result));
       } catch (error) {
         console.log("Failed to fetch total supply", error);
       }
     }
 
     fetchTotalSupply();
-  }, []);
+  }, [donors]);
 
   useEffect(() => {
     const unwatch = publicClient.watchContractEvent({
-      address: "0x0",
+      address: "0x992d9e030fF8187F950298f89Fb52745764bdEEF",
       abi: circlesTreeABI,
       eventName: "Transfer",
       pollingInterval: 2000,
@@ -34,8 +34,8 @@ function useDonations() {
         setDonors((prevDonors) => {
           const newDonors = [...prevDonors];
           logs.forEach((log) => {
-            if (log.args.from && !prevDonors.includes(log.args.from)) {
-              newDonors.push(log.args.from);
+            if (log.args.to && !prevDonors.includes(log.args.to)) {
+              newDonors.push(log.args.to);
             }
           });
           return newDonors;
