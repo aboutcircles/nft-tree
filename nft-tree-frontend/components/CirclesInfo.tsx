ab65e2bf116a96575bcc5b17400a110b2cc2bb19
Luka la d <lukas-8@live.fr>
2024-04-27 22:23:11 +0200

    feat: fetch all datas from server

---
+++ nft-tree-frontend/components/CirclesInfo.tsx
@@ -0,0 +1,19 @@
+
+import Circles from "./circles";
+
+interface CirclesInfoProps {
+  supply: number;
+}
+
+export default function CirclesInfo({ supply }: CirclesInfoProps) {
+  // const { totalSupply } = useDonations();
+  return (
+    <div className="flex flex-col items-end lg:items-start">
+      <p className="text-sm lg:text-2xl">NFTS MINTED</p>
+      <p className="text-lg lg:text-4xl">{supply}/1000</p>
+      <div className="w-28 h-28 lg:w-44 lg:h-44 border-2 mt-2">
+        <Circles />
+      </div>
+    </div>
+  );
+}
