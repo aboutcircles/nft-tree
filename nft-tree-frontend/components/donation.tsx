7fd989d3792be61fc391e0ae33a16e0d3ab9529a
Luka la d <lukas-8@live.fr>
2024-05-01 23:00:56 +0200

    feat: add dashboard page

--- nft-tree-frontend/components/donation.tsx
+++ nft-tree-frontend/components/donation.tsx
@@ -8,11 +8,11 @@ export default function Donation({ address, imageUrl, username }: Donor) {
     <Link className="w-full flex justify-between items-center border-t border-l border-r border-b lg:border-b-0 lg:last:border-b border-slate-300 p-1 lg:p-2 hover:cursor-pointer" href={"https://circles.garden/profile/" + address} target="_blank">
       {imageUrl ? <Image src={imageUrl} alt={""} width={25} height={25} /> : <Image src={"/profileDefault.jpg"} alt={"profileDefault"} width={25} height={25} className="border-white border" />}
       <div className="flex w-full flex-col ml-2">
-        <p>From</p>
-        <p className="text-[10px]">{truncateAddress(address)}</p>
+        <p className="text-[10px]">From</p>
+        <p className="text-[5px] ">{truncateAddress(address)}</p>
         {/* <p className="text-[10px]">{username}</p> */}
       </div>
-      <p className="text-nowrap">50 CRC</p>
+      <p className="text-nowrap text-[12px]">50 CRC</p>
     </Link>
   );
 }
