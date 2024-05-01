ebeaf451d45478eaf5006481ba609161149a866e
Luka la d <lukas-8@live.fr>
2024-05-01 20:55:23 +0200

    fix: layout update on screen ratio

--- nft-tree-frontend/components/donation.tsx
+++ nft-tree-frontend/components/donation.tsx
@@ -6,13 +6,13 @@ import Link from "next/link";
 export default function Donation({ address, imageUrl, username }: Donor) {
   return (
     <Link className="w-full flex justify-between items-center border-t border-l border-r border-b lg:border-b-0 lg:last:border-b border-slate-300 p-1 lg:p-2 hover:cursor-pointer" href={"https://circles.garden/profile/" + address} target="_blank">
-      {imageUrl ? <Image src={imageUrl} alt={""} /> : ""}
-      <div className="flex flex-col">
+      {imageUrl ? <Image src={imageUrl} alt={""} width={25} height={25} /> : <Image src={"/profileDefault.jpg"} alt={"profileDefault"} width={25} height={25} className="border-white border" />}
+      <div className="flex w-full flex-col ml-2">
         <p>From</p>
         <p className="text-[10px]">{truncateAddress(address)}</p>
         {/* <p className="text-[10px]">{username}</p> */}
       </div>
-      50 CRC
+      <p className="text-nowrap">50 CRC</p>
     </Link>
   );
 }
--- nft-tree-frontend/components/donations.tsx
+++ nft-tree-frontend/components/donations.tsx
@@ -1,4 +1,3 @@
-import { Address } from "viem";
 import Donation from "./donation";
 import { Donor } from "@/actions/fetchDatas";
 
@@ -8,7 +7,7 @@ interface DonationsProps {
 
 export default function Donations({ donors }: DonationsProps) {
   return (
-    <div className="w-full max-w-52 lg:p-4">
+    <div className="w-full max-w-60 lg:p-4">
       <p className="mb-2">Recent Donations</p>
       <div className="w-full flex gap-x-1 lg:flex-col text-xs">
         {donors.slice(0, 10).map((donor, index) => (
