d532fe6174d53b9b9fcac40ea5dc6d617a200f3c
Luka la d <lukas-8@live.fr>
2024-05-02 13:45:19 +0200

    fix: render entire tree on every frame

--- nft-tree-frontend/components/tree.tsx
+++ nft-tree-frontend/components/tree.tsx
@@ -18,114 +18,119 @@ export type Node = {
   children: Set<Address>;
 };
 
-export default function Tree({nodes}: TreeProps) {
+export default function Tree({ nodes }: TreeProps) {
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
 
   useEffect(() => {
-    const canvaWrapper = document.getElementById('CanvaWrapper');
+    const canvaWrapper = document.getElementById("CanvaWrapper");
     if (canvaWrapper) {
       console.log(canvaWrapper.offsetWidth, canvaWrapper.offsetHeight);
       setDimensions({
         width: canvaWrapper.offsetWidth,
-        height: canvaWrapper.offsetHeight
+        height: canvaWrapper.offsetHeight,
       });
     }
 
     const handleResize = () => {
       setDimensions({
         width: canvaWrapper?.offsetWidth || 0,
-        height: canvaWrapper?.offsetHeight || 0
+        height: canvaWrapper?.offsetHeight || 0,
       });
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   const sketch: Sketch = (p5) => {
-    let paths: Pathfinder[];
+    let rootPathfinder: Pathfinder;
+    let allPathfinder: Pathfinder[] = [];
 
     class Pathfinder {
       address: Address;
-      lastLocation: p5.Vector;
       location: p5.Vector;
       velocity: p5.Vector;
       diameter: number;
-      lifetime: number;
       level: number;
 
       constructor(address: Address, parent?: Pathfinder) {
+        this.address = address;
         if (parent) {
-          this.address = address;
           this.location = parent.location.copy();
-          this.lastLocation = parent.lastLocation.copy();
           this.velocity = parent.velocity.copy();
-          this.velocity.rotate(p5.random(-p5.QUARTER_PI / 2, p5.QUARTER_PI / 2));
-          this.velocity.setMag(p5.random(5, 10));
-          this.diameter = parent.diameter * 0.9;
-          this.lifetime = 30 - parent.level * 3;
+          this.velocity.rotate(p5.random(-p5.QUARTER_PI / 3, p5.QUARTER_PI / 3));
           this.level = parent.level + 1;
+          this.velocity.setMag(p5.random(150 * Math.pow(0.8, this.level), 170 * Math.pow(0.8, this.level)));
         } else {
-          this.address = address;
           this.location = p5.createVector(p5.width / 2, p5.height);
-          this.lastLocation = this.location.copy();
           this.velocity = p5.createVector(0, -p5.random(1, 5));
-          this.diameter = p5.random(0.8, 1);
-          this.lifetime = 30;
           this.level = 1;
         }
+        this.diameter = Math.max(0.8, 1 - 0.1 * this.level);
       }
 
       update() {
-        this.lastLocation.set(this.location.x, this.location.y);
         let bump = p5.createVector(p5.random(-1, 1), p5.random(-1, 1));
         bump.mult(0.1 * (1 / this.level));
         this.velocity.add(bump);
-        this.velocity.limit(5);
         this.location.add(this.velocity);
-        this.lifetime--;
       }
 
+      // adjust() {
+      //   let drift = p5.createVector(p5.random(-0.2, 0.2), p5.random(-0.2, 0.2));
+      //   this.location.add(drift);
+      // }
+
       draw() {
+        // this.adjust();
+        let bottomColor = p5.color(231, 179, 210);
+        let topColor = p5.color(141, 132, 139);
+        let inter = p5.map(this.location.y, dimensions.height, 0, 0, 1);
+        let c = p5.lerpColor(bottomColor, topColor, inter);
         p5.strokeWeight(this.diameter);
-        p5.stroke(217, 160, 235, 200);
-        p5.line(this.lastLocation.x, this.lastLocation.y, this.location.x, this.location.y);
+        p5.stroke(c);
+        p5.line(this.location.x - this.velocity.x, this.location.y - this.velocity.y, this.location.x, this.location.y);
       }
     }
 
-    function drawTree() {
-      if (paths) {
-        paths.forEach((path, index) => {
-          path.draw();
-          path.update();
-          if (path.lifetime <= 0) {
-            const node = nodes.find((n) => n.id === path.address);
-            if (node && node.children.size > 0) {
-              let pathsTemp = Array.from(node.children).map((childAddress) => new Pathfinder(childAddress, path));
-              paths.push(...pathsTemp);
-            }
-            paths.splice(index, 1);
+    function setupTree(node: Node, pathfinder: Pathfinder) {
+      if (node && node.children.size > 0) {
+        node.children.forEach((childAddress) => {
+          let childNode = nodes.find((n) => n.id === childAddress);
+          if (childNode) {
+            let newBranch = new Pathfinder(childAddress, pathfinder);
+            newBranch.update();
+            allPathfinder.push(newBranch);
+            setupTree(childNode, newBranch);
           }
         });
       }
     }
 
     p5.setup = () => {
-      p5.createCanvas(dimensions.width, dimensions.height); //TODO find a way to resize responsively the canva
+      p5.createCanvas(dimensions.width, dimensions.height);
       p5.smooth();
+      p5.randomSeed(95);
       const rootNode = nodes.find((n) => n.id === treasuryAddress);
       if (rootNode) {
-        paths = Array.from(rootNode.children).map((childAddress) => new Pathfinder(childAddress));
+        rootPathfinder = new Pathfinder(treasuryAddress);
+        setupTree(rootNode, rootPathfinder);
       }
     };
 
     p5.draw = () => {
-      // p5.background(0, 30);
-      drawTree();
+      p5.background(0);
+      allPathfinder.forEach((path) => {
+        path.draw();
+      });
     };
   };
 
-  return <div className="w-full h-full" id="CanvaWrapper"><NextReactP5Wrapper sketch={sketch} /></div>;
+  return (
+    <div className="w-full h-full" id="CanvaWrapper">
+      <NextReactP5Wrapper sketch={sketch} />
+    </div>
+  );
 }
