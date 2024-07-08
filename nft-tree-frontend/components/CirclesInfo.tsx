81836dcc11be99772401b183617cb42edab2deed
Luka la d <lukas-8@live.fr>
2024-07-08 14:49:47 +0200

    fix: update qr code dimension

--- nft-tree-frontend/components/CirclesInfo.tsx
+++ nft-tree-frontend/components/CirclesInfo.tsx
@@ -7,7 +7,7 @@ export default function CirclesInfo() {
   const { supply } = useTreeData();
   return (
     <div className="flex justify-start md:justify-between gap-x-4 items-center portrait:w-screen lanscape:w-full md:w-full portrait:md:items-end border-2 landscape:border-0 md:border-0 landscape:flex-col md:flex-col portrait:md:flex-col-reverse landscape:items-start p-2 md:p-0">
-      <div className="w-12 h-12 bg-[url('/nftIcon.gif')] bg-cover bg-center landscape:w-28 landscape:h-28 landscape:lg:w-[180px] landscape:lg:h-[180px] portrait:lg:w-[240px] portrait:lg:h-[240px] landscape:md:w-[120px] landscape:md:h-[120px] portrait:md:w-[180px] portrait:md:h-[180px] border-2 lg:mt-2">
+      <div className="w-12 h-12 bg-[url('/nftIcon.gif')] bg-cover bg-center landscape:w-28 landscape:h-28 landscape:lg:w-[180px] landscape:lg:h-[180px] portrait:lg:w-[240px] portrait:lg:h-[240px] landscape:md:w-[180px] landscape:md:h-[180px] portrait:md:w-[180px] portrait:md:h-[180px] border-2 lg:mt-2">
         {/* <CirclesIcon /> */}
       </div>
       <div className="flex flex-col items-end landscape:items-start">
