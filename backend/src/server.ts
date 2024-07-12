2db5c8fed8fc828794aaa807270a931c355088af
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2024-07-12 09:09:57 +0200

    change port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,55 +1,55 @@
-import express, { Request, Response } from "express";
-import sqlite3 from "sqlite3";
-import { spawn } from "child_process";
-import { db } from "./db/models/index.js";
-import { Op } from "sequelize";
-import cors from "cors";
+import express, { Request, Response } from 'express';
+import sqlite3 from 'sqlite3';
+import { spawn } from 'child_process';
+import { db } from './db/models/index.js';
+import { Op } from 'sequelize';
+import cors from 'cors';
 import {
   getStatusMinting,
   initializeMintingStatusDB,
-} from "./processTransfers/mintingStatus.js";
+} from './processTransfers/mintingStatus.js';
 
 const app = express();
 app.use(cors());
-const PORT = 8000;
+const PORT = 8080;
 
 (async () => {
   try {
     await db.sequelize.sync();
     await initializeMintingStatusDB();
-    console.log("Database initialized successfully.");
+    console.log('Database initialized successfully.');
     // Start your server here or perform other database operations
 
     // Start the background task as a separate process
-    const background = spawn("node", ["dist/processTransfers/index.js"]);
+    const background = spawn('node', ['dist/processTransfers/index.js']);
 
-    background.stdout.on("data", (data: Buffer) => {
+    background.stdout.on('data', (data: Buffer) => {
       console.log(`Minting task says: ${data.toString()}`);
     });
 
-    background.stderr.on("data", (data: Buffer) => {
+    background.stderr.on('data', (data: Buffer) => {
       console.error(`Minting task had an error: ${data}`);
     });
 
-    background.on("close", (code: number) => {
+    background.on('close', (code: number) => {
       console.log(`Minting task exited with code: ${code}`);
     });
   } catch (error) {
-    console.error("Failed to initialize database:", error);
+    console.error('Failed to initialize database:', error);
   }
 })();
 
 const dbTest = new sqlite3.Database(
-  "./test.db",
+  './test.db',
   sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
   (err: Error | null) => {
     if (err) {
-      console.error("Error opening database", err);
+      console.error('Error opening database', err);
     }
   }
 );
 
-app.get("/tree-data", (req: Request, res: Response) => {
+app.get('/tree-data', (req: Request, res: Response) => {
   // console.log("🟢 GET /tree-data");
   const lastFetchedId = req.query.id || 0;
   db.models.TreeData.findAll({
@@ -58,26 +58,26 @@ app.get("/tree-data", (req: Request, res: Response) => {
         [Op.gt]: lastFetchedId,
       },
     },
-    order: [["id", "ASC"]],
+    order: [['id', 'ASC']],
   })
     .then((rows: any) => {
       res.json(rows);
     })
     .catch((err: { message: any }) => {
-      res.status(500).send("Failed to retrieve data from database.");
+      res.status(500).send('Failed to retrieve data from database.');
       console.error(err.message);
     });
 });
 
-app.get("/tree-test", (req: Request, res: Response) => {
+app.get('/tree-test', (req: Request, res: Response) => {
   // console.log("🟢 GET /tree-test");
   const lastFetchedId = req.query.id || 0;
   dbTest.all(
-    "SELECT * FROM treeData WHERE id > ?",
+    'SELECT * FROM treeData WHERE id > ?',
     [lastFetchedId],
     (err: Error | null, rows: any[]) => {
       if (err) {
-        res.status(500).send("Failed to retrieve data from database.");
+        res.status(500).send('Failed to retrieve data from database.');
         console.error(err.message);
       } else {
         res.json(rows);
@@ -86,19 +86,19 @@ app.get("/tree-test", (req: Request, res: Response) => {
   );
 });
 
-app.get("/db-transfers", async (req: Request, res: Response) => {
+app.get('/db-transfers', async (req: Request, res: Response) => {
   // console.log("🟢 GET /db-transfers");
   db.models.Transfer.findAll()
     .then((rows: any) => {
       res.json(rows);
     })
     .catch((err: { message: any }) => {
-      res.status(500).send("Failed to retrieve data from database.");
+      res.status(500).send('Failed to retrieve data from database.');
       console.error(err.message);
     });
 });
 
-app.get("/minting-status", async (_: Request, res: Response) => {
+app.get('/minting-status', async (_: Request, res: Response) => {
   // console.log("🟢 GET /minting-status");
   try {
     const status = await getStatusMinting();
@@ -112,11 +112,11 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-process.on("SIGINT", () => {
+process.on('SIGINT', () => {
   db.sequelize
     .close()
     .then(() => {
-      console.log("Database connection closed.");
+      console.log('Database connection closed.');
       process.exit(0);
     })
     .catch((err) => {
